7d538571d0197b3fb32ccc4eaac998392cdfe9cd
wangyan <wangyan@xishuhq.com>
2023-08-10 09:28:45 +0800

    [feat(md)]: add markdown preview

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,19 @@
+return {
+	{
+		"iamcco/markdown-preview.nvim",
+		build = "cd app && npm install",
+		ft = "markdown",
+		lazy = true,
+		keys = { { "gm", "<cmd>MarkdownPreviewToggle<cr>", desc = "Markdown Preview" } },
+		config = function()
+			vim.g.mkdp_auto_close = true
+			vim.g.mkdp_open_to_the_world = false
+			vim.g.mkdp_open_ip = "127.0.0.1"
+			vim.g.mkdp_port = "8888"
+			vim.g.mkdp_browser = ""
+			vim.g.mkdp_echo_preview_url = true
+			vim.g.mkdp_page_title = "${name}"
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+	},
+}
